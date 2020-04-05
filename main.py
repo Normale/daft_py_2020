1cb531020c035cba0e70c6e9edd94df6d3462abe
Normal <bartek.dybcio1@gmail.com>
2020-04-05 17:31:53 +0200

    Task 3 + tests

--- main.py
+++ main.py
@@ -8,15 +8,33 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
+@app.get("/")
+def root():
+    return {"message": "Hello World during the coronavirus pandemic!"}
+
+
 @app.get("/method")
-def get_response():
+async def get_response():
     return {"method": "GET"}
 
 @app.post("/method")
-def post_response():
+async def post_response():
     return  {"method": "POST"}
 
     
 @app.put("/method")
-def put_response():
+async def put_response():
     return  {"method": "PUT"}
+
+class PatientRequest(BaseModel):
+    name: str
+    surename: str
+
+class PatientResponse(BaseModel):
+    id: int
+    patient: Dict
+@app.post("/patient", response_model=PatientResponse)
+def new_patient(rq: PatientRequest):
+    new_patient.id += 1
+    return PatientResponse(patient=rq.dict(), id = new_patient.id)
+new_patient.id = -1
--- test_main.py
+++ test_main.py
@@ -9,18 +9,28 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == {"message": "Hello World during the coronavirus pandemic!"} 
+
+
+def test_methods():
+    response = client.get(f"/method")
+    assert response.status_code == 200
+    assert response.json() == {'method': "GET"}
+    response = client.post(f"/method")
+    assert response.status_code == 200
+    assert response.json() == {'method': "POST"}
+    response = client.put(f"/method")
+    assert response.status_code == 200
+    assert response.json() == {'method': "PUT"}
 
 
 @pytest.mark.parametrize("name", ['Zenek', 'Marek', 'Alojzy Niezdąży'])
-def test_hello_name(name):
-    # name = 'elo'
-    response = client.get(f"/hello/{name}")
-    assert response.status_code == 200
-    assert response.json() == {'msg': f"Hello {name}"}
+@pytest.mark.parametrize("surename", ['Zenekx', 'Marekx', 'Alojzy Niezdyx'])
+def test_patients(name, surename):
+    response = client.post("/patient", json={"name": f"{name}", 
+                                            "surename": f"{surename}"})
+    assert response.json() == {"id": test_patients.callCount, "patient": {"name": f"{name}", 
+                                                                "surename": f"{surename}"}}
+    test_patients.callCount += 1
 
-
-def test_receive_something():
-    response = client.post("/dej/mi/coś", json={'first_key': 'some_value'})
-    assert response.json() == {"received": {'first_key': 'some_value'},
-                             "constant_data": "python jest super"}
+test_patients.callCount = 0
