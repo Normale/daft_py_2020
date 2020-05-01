d9e62c81ead6aac383dde1500befc47de372ce81
Normal <bartek.dybcio1@gmail.com>
2020-05-01 23:27:45 +0200

    DATABASES START

--- main.py
+++ main.py
@@ -4,21 +4,16 @@ from typing import Dict, Optional
 from fastapi import Depends, FastAPI, Response, status, Request, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.security import APIKeyCookie, HTTPBasic, HTTPBasicCredentials
-from jose import jwt
 from pydantic import BaseModel
 from starlette.responses import RedirectResponse
 
 
-class Patient(BaseModel):
-    name: str
-    surname: str
+import aiosqlite
 
 
 class DaftAPI(FastAPI):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.counter: int = 0
-        self.storage: Dict[int, Patient] = {}
         self.security = HTTPBasic(auto_error=False)
         self.secret_key = "kluczyk"
         self.API_KEY = "session"
@@ -29,84 +24,28 @@ class DaftAPI(FastAPI):
 app = DaftAPI()
 
 
-def is_logged(session: str = Depends(app.cookie_sec), silent: bool = False):
-    try:
-        payload = jwt.decode(session, app.secret_key)
-        return payload.get("magic_key")
-    except Exception:
-        pass
-
-    if silent:
-        return False
-
-    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+@app.on_event("startup")
+async def startup():
+    app.db_connection = await aiosqlite.connect('db/chinook.db')
 
 
-def authethicate(credentials: Optional[HTTPBasicCredentials] = Depends(app.security)):
-    if not credentials:
-        return False
+@app.on_event("shutdown")
+async def shutdown():
+    await app.db_connection.close()
 
-    correct_username = secrets.compare_digest(credentials.username, "trudnY")
-    correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
 
-    if not (correct_username and correct_password):
-        return False
-    return True
+
+
+# # # HOW TO USE ASYNC SQLITE
+# @app.get("/data")
+#  async def root():
+#     cursor = await app.db_connection.execute("....")
+#     data = await cursor.fetchall()
+#     return {"data": data}
 
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello World during the coronavirus pandemic!"}
-
-
-@app.get("/welcome")
-def welcome(request: Request, is_logged: bool = Depends(is_logged)):
-    return app.templates.TemplateResponse(
-        "welcome.html", {"request": request, "user": "trudnY"}
-    )
-
-
-@app.post("/login")
-async def login_basic(auth: bool = Depends(authethicate)):
-    if not auth:
-        response = Response(headers={"WWW-Authenticate": "Basic"}, status_code=401)
-        return response
-
-    response = RedirectResponse(url="/welcome")
-    token = jwt.encode({"magic_key": True}, app.secret_key)
-    response.set_cookie("session", token)
-    return response
-
-
-@app.post("/logout")
-async def logout(is_logged: bool = Depends(is_logged)):
-    response = RedirectResponse(url="/")
-    response.delete_cookie("session")
-    return response
-
-
-@app.post("/patient")
-def add_patient(patient: Patient, is_logged: bool = Depends(is_logged)):
-    app.storage[app.counter] = patient
-    response = RedirectResponse(url=f"/patient/{app.counter}")
-    app.counter += 1
-    return response
-
-
-@app.get("/patient")
-def show_patients(is_logged: bool = Depends(is_logged)):
-    return app.storage
-
-
-@app.get("/patient/{pk}")
-def show_patient(pk: int, is_logged: bool = Depends(is_logged)):
-    if pk in app.storage:
-        return app.storage.get(pk)
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-@app.delete("/patient/{pk}")
-def delte_patient(pk: int, is_logged: bool = Depends(is_logged)):
-    if pk in app.storage:
-        del app.storage[pk]
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+async def get_tracks(page: int = 0, per_page: int = 10):
+    cursor = await app.db_connection.execute(f"SELECT * FROM tracks LIMIT {per_page} OFFSET {per_page * page}")
+    data = await cursor.fetchall()
+    return data
